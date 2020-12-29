1aea62bb4e86dc6b5c280f216c140994b57740a7
LJSo0o <53509736+LJSo0o@users.noreply.github.com>
2020-12-29 18:29:30 +0900

    test

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager vp = findViewById(R.id.viewpager);
+        ViewPager_Adapter adapter = new ViewPager_Adapter(getSupportFragmentManager());
+        vp.setAdapter(adapter);
+
+        //아래부터 tab_layout이랑 viewpager 연동하는 코드
+        TabLayout tab = findViewById(R.id.tab_layout);
+        tab.setupWithViewPager(vp);
+
     }
-}
+} // ViewPager, ViewPager_Adapter 생성 후 연결
