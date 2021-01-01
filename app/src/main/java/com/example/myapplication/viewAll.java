e6e477e62ee7df2ead47a68ed5b10612cf021162
younho117 <66814513+younho117@users.noreply.github.com>
2021-01-02 01:15:33 +0900

    tab2_design_continue

--- app/src/main/java/com/example/myapplication/viewAll.java
+++ app/src/main/java/com/example/myapplication/viewAll.java
@@ -30,9 +30,12 @@ public class viewAll extends AppCompatActivity {
 
     static int check;
 
-    int[] img_id = {R.id.imglist1, R.id.imglist2, R.id.imglist3, R.id.imglist4, R.id.imglist5, R.id.imglist6, R.id.imglist7, R.id.imglist8, R.id.imglist9, R.id.imglist10};
-    int[] img_src = {R.drawable.white, R.drawable.green, R.drawable.sky, R.drawable.mountain, R.drawable.rainbow, R.drawable.clock, R.drawable.red, R.drawable.purple, R.drawable.computer, R.drawable.orange};
-    int[] heart_id = {R.id.heart1, R.id.heart2, R.id.heart3, R.id.heart4, R.id.heart5, R.id.heart6, R.id.heart7, R.id.heart8, R.id.heart9, R.id.heart10};
+    int[] img_id = {R.id.imglist1, R.id.imglist2, R.id.imglist3, R.id.imglist4, R.id.imglist5, R.id.imglist6, R.id.imglist7, R.id.imglist8, R.id.imglist9, R.id.imglist10,
+            R.id.imglist11, R.id.imglist12, R.id.imglist13, R.id.imglist14, R.id.imglist15, R.id.imglist16, R.id.imglist17, R.id.imglist18, R.id.imglist19, R.id.imglist20};
+    int[] img_src = {R.drawable.white, R.drawable.green, R.drawable.sky, R.drawable.mountain, R.drawable.rainbow, R.drawable.clock, R.drawable.red, R.drawable.purple, R.drawable.computer, R.drawable.orange,
+            R.drawable.cat1, R.drawable.cat2, R.drawable.cat3, R.drawable.cat4, R.drawable.cat5, R.drawable.cat6, R.drawable.goose1, R.drawable.cat7, R.drawable.cat8, R.drawable.cat9};
+    int[] heart_id = {R.id.heart1, R.id.heart2, R.id.heart3, R.id.heart4, R.id.heart5, R.id.heart6, R.id.heart7, R.id.heart8, R.id.heart9, R.id.heart10,
+            R.id.heart11, R.id.heart12, R.id.heart13, R.id.heart14, R.id.heart15, R.id.heart16, R.id.heart17, R.id.heart18, R.id.heart19, R.id.heart20,};
 
 
     @Override
